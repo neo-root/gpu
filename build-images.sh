c87d40baf10e7164605c162fed06a0b1afad2e6d
neo-root <61357666+neo-root@users.noreply.github.com>
2021-05-01 23:00:02 +0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -51,17 +51,17 @@ fi
 
 if [ -z ${nopull} ]; then
   echo -e "${GREEN}Pulling latest upstream image...${NC}"
-  docker pull blockcollider/bcnode:latest
+  docker lgray/bcnode:last
 echo
 fi
 
 echo -e "${GREEN}Building new image...${NC}"
-docker build -t local/bcnode -f Dockerfile.bcnode .
+docker build -t lgray/bcnode:last -f Dockerfile.bcnode .
 echo
 
 if [ -z ${nopurge} ]; then
   echo -e "${GREEN}Removing original bcnode image...${NC}"
-  docker rmi blockcollider/bcnode:latest
+  docker rmi lgray/bcnode:last
   echo
 fi
 
